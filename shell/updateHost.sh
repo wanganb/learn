027f28e368c32421bd0ddef9f7219c16ee54e587
wangyi19 <wangyi19@staff.weibo.com>
2017-11-28 17:52:13 +0800

    修改为支持批量更改host

--- shell/updateHost.sh
+++ shell/updateHost.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 hostfile="/etc/hosts"
-hostname="recommend.match.sina.com.cn"
-hostip=`dig ${hostname} +short|sed -n '1p'`
-if [ -n "${hostip}" ]
-then
-	existhost=`grep ${hostname} ${hostfile}`
-	if [ -n "${existhost}" ]
+hostnames="recommend.match.sina.com.cn weibointra.match.sina.com.cn"
+for hostname in ${hostnames};
+do
+	hostip=`dig ${hostname} +short|grep -E -o '([0-9]{1,3}[\.]){3}[0-9]{1,3}'|sed -n '1p'`
+	if [ -n "${hostip}" ]
 	then
-		#update
-		sed -i "s/[.0-9]\{7,15\} \(${hostname}\)/${hostip} \1/" ${hostfile}
-	else
-		#add
-		sed -i "\$a${hostip} ${hostname}" ${hostfile}
+		existhost=`grep ${hostname} ${hostfile}`
+		if [ -n "${existhost}" ]
+		then
+			#update
+			sed -i "s/[.0-9]\{7,15\} \(${hostname}\)/${hostip} \1/" ${hostfile}
+		else
+			#add
+			sed -i "\$a${hostip} ${hostname}" ${hostfile}
+		fi
 	fi
-fi
+done
